eab71d62bb06d84ab87eadd25acb0b1fb21e0b88
externalreality <ejones@protonmail.com>
2021-03-06 22:13:44 -0500

    Organize project.

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,18 @@ extern crate clap;
 
 #[macro_use]
 extern crate prettytable;
+
 use prettytable::Table;
 
 use clap::App;
 use openapi::apis::{build_type_api, configuration::Configuration};
-use serde_derive::{Deserialize, Serialize};
 use std::collections::BTreeSet;
-use std::env;
 use std::fs::File;
-use strum_macros::EnumString;
 use tokio;
 
-#[derive(Serialize, Deserialize, EnumString, Debug)]
-enum Provider {
-    TeamCity,
-    GitLab,
-}
+mod provider;
 
-#[derive(Deserialize, Serialize, Debug)]
-struct ProviderConfig {
-    provider: Provider, 
-    url: String,
-    api_token: Option<String>,
-}
+pub use provider::{Provider, ProviderType};
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
@@ -56,7 +45,7 @@ fn handle_provider(sc: &clap::ArgMatches<'_>) {
 
 fn handle_enable(sc: &clap::ArgMatches<'_>) {
     let input = sc.value_of("provider").unwrap();
-    let _: Provider = input.parse().unwrap();
+    let _: ProviderType = input.parse().unwrap();
     let file = File::open("cider.yaml").unwrap();
     let len = file.metadata().unwrap().len();
     let mut providers: BTreeSet<String> = BTreeSet::new();
@@ -73,7 +62,7 @@ fn handle_enable(sc: &clap::ArgMatches<'_>) {
 
 fn handle_list(_: &clap::ArgMatches<'_>) {
     let file = File::open("cider.yaml").expect("failed opening file");
-    let providers: Vec<Provider> = serde_yaml::from_reader(file).unwrap();
+    let providers: Vec<ProviderType> = serde_yaml::from_reader(file).unwrap();
     let mut table = Table::new();
     table.add_row(row!["name"]);
     for p in providers {
@@ -92,8 +81,8 @@ fn handle_pipeline(sc: &clap::ArgMatches<'_>) {
 #[tokio::main]
 async fn handle_pipeline_list(_: &clap::ArgMatches<'_>) {
     let file = File::open("cider.yaml").expect("failed opening file");
-    let providers: Vec<Provider> = serde_yaml::from_reader(file).unwrap();
-    let cfgs = load_provider_configs(providers);
+    let providers: Vec<ProviderType> = serde_yaml::from_reader(file).unwrap();
+    let cfgs = provider::configuration::load_provider_configs(providers);
 
     let mut table = Table::new();
     table.add_row(row!["name", "provider", "id", "project"]);
@@ -104,8 +93,8 @@ async fn handle_pipeline_list(_: &clap::ArgMatches<'_>) {
         let m = build_type_api::get_all_build_types(&rc, None, None)
             .await
             .unwrap();
-        
-        let pipelines = m.build_type.unwrap(); 
+
+        let pipelines = m.build_type.unwrap();
 
         for p in pipelines {
             table.add_row(row![
@@ -116,35 +105,24 @@ async fn handle_pipeline_list(_: &clap::ArgMatches<'_>) {
             ]);
         }
     }
-
     table.printstd();
 }
 
 #[tokio::main]
 async fn handle_project_list(_: &clap::ArgMatches<'_>) {
-    let cfg = load_provider_config(Provider::TeamCity);
+    let cfg = provider::configuration::load_provider_config(ProviderType::TeamCity);
     let mut c: Configuration = Configuration::new();
     c.bearer_access_token = cfg.api_token;
-    let m = openapi::apis::project_api::get_all_projects(&c, None, None).await;
-    println!("{:?}", m);
-}
+    let _: provider::teamcity::TeamCity = provider::Provider::new();
 
-fn load_provider_configs(providers: Vec<Provider>) -> Vec<ProviderConfig> {
-    let mut cfgs: Vec<ProviderConfig> = vec![];
-    for p in providers {
-        cfgs.push(load_provider_config(p));
-    }
-    cfgs
-}
-
-fn load_provider_config(provider: Provider) -> ProviderConfig {
-    let mut filename = format!("./cider_config/{:?}.json", provider);
-    filename = filename.to_lowercase();
-    let file = File::open(filename).expect("failed opening file");
-    let mut cfg: ProviderConfig = serde_yaml::from_reader(file).unwrap();
-    let token_var = format!("{:?}_token", provider).to_uppercase();
-    let token = env::var(token_var).expect("token env var not found");
-    cfg.api_token = Some(token);
-    cfg.provider = provider;
-    cfg
+    // let mut table = Table::new();
+    // table.add_row(row!["name", "provider", "id"]);
+    // for p in res.project.unwrap() {
+    //     table.add_row(row![
+    //         p.name.unwrap(),
+    //         format!("{:?}", cfg.provider),
+    //         p.id.unwrap(),
+    //     ]);
+    // }
+    // table.printstd();
 }
